29ce7ab5f0b971f011401b70f462e435ce76b91c
CecemelDev <cedricthauvoye1@gmail.com>
2022-01-06 18:33:09 +0100

    Update mysql_pool.js

--- src/mysql_pool.js
+++ src/mysql_pool.js
@@ -10,11 +10,9 @@ const pool = mysql.createPool({
 
 module.exports.pool = pool
 
-module.exports = {
-    createQuery: (sql, data, callBack) => {
-        pool.query(sql, data, (err, res, fields) => {
-            if(err) return callBack(err)
-            return callBack(null, res)
-        })
-    }
+module.exports.createQuery = (sql, data, callBack) => {
+    pool.query(sql, data, (err, res, fields) => {
+        if(err) return callBack(err)
+        return callBack(null, res)
+    })
 }
