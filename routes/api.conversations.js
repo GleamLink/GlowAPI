e8fbe3f7a93a7139e02ff4c561c435cf4c792815
CecemelDev <cedricthauvoye1@gmail.com>
2022-02-19 11:32:14 +0100

    Added getConversations and more

--- routes/api.conversations.js
+++ routes/api.conversations.js
@@ -1,43 +1,48 @@
 const { Router } = require('express');
-const { postConversation, createConversation, getConversation } = require('../src/utils/conversations');
+const { postConversation, createConversation, getConversations, getConversation, getConversationById } = require('../src/utils/conversations');
 const { verifyAccessToken, getUser } = require('../src/util');
+const { getMessages } = require('../src/utils/messages');
 
 module.exports.Router = class Routes extends Router {
     constructor() {
         super()
 
+        // GEt /conversations/ - Returns a list of conversations where the user is in
         this.get('/', verifyAccessToken, (req, res) => {
+            getConversations(req.user.userId, (err, resu) => {
+                if(err) return res.status(500).send(err)
+                res.send(resu)
+            })
         })
 
         // POST /conversations/ - Creates a conversation with specific user if no exists
         this.post('/', verifyAccessToken, (req, res) => {
-            if(!req.body.senderId) return res.status(500).send({"message": "Sender required."})
+            if(!req.body.userId) return res.status(500).send({"message": "User required."})
         });
 
         // GET /conversations/:userId/ - Returns the conversation id with specific user
         this.get('/:userId', verifyAccessToken, (req, res) => {
             if(!req.params.userId) return res.status(500).send({"message": "User required."})
+            if(req.params.userId === req.user.userId) return res.status(401).send({"message": "You can't get conversations with yourself."})
             getUser(req.params.userId, (err, resu) => {
                 if(err) return res.status(500).send(err)
                 getConversation(req.params.userId, req.user.userId, (err, resu) => {
-                    console.log("ABCDEF: ",
-                        req.params.userId,
-                        req.user.userId,
-                        err,
-                        resu[0]
-                    )
                     if(err) return res.status(500).send(err)
-                    if(!resu.length) {
-                        createConversation(req.user.userId, req.params.userId, (err, resu2) => {
-                            if(err) return res.status(500).send(err)
-                            return res.send(resu2)
-                        })
-                        // return res.status(500).send({"message": "We don't know why, but there is no conversation existing with you and the user."})
-                    } else if(resu.length > 1) {
-                        return res.send(resu)
-                    } else {
-                        return res.send(resu[0])
-                    }
+                    return res.send(resu)
+                })
+            })
+        })
+
+        // GET /conversations/:conversationId/messages - Returns all messages from specific conversation
+        this.get('/:convId/messages', verifyAccessToken, (req, res) => {
+            getConversationById(req.params.convId, (err, resu) => {
+                console.log(resu)
+                if(!resu) return res.json({"message": "No conversation with this id."})
+                if(!resu.members.includes(req.user.userId)) return res.status(401).send({"message": "Unauthorized to see this conversation."})
+                getMessages(req.params.convId, (err, resu) => {
+                    if(err) return res.status(500).send(err)
+                    if(!resu.length) return res.json([])
+                    res.send(resu)
                 })
             })
             
