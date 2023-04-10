47024d79d26dcf5c90f54f2be86ce48422506100
C3cemelDev <c3cemel@gmail.com>
2023-04-10 18:29:58 +0200

    Added multer, body-parser and jsonwebtoken

--- index.js
+++ index.js
@@ -1,39 +1,73 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-var cookieParser = require('cookie-parser')
-const jwt = require('jsonwebtoken')
+var cookieParser = require("cookie-parser")
+const jwt = require("jsonwebtoken")
 const cors = require("cors")
+const multer = require("multer")
+const mime = require("mime-types")
+const path = require("path")
 
-require('dotenv').config()
-const fs = require('fs')
-const port = process.env.GLOW_PORT
-const bodyParser = require('body-parser')
-const { imgToBase64 } = require('./src/utils/avatar')
+require("dotenv").config()
+const fs = require("fs")
+const port = process.env.PORT
+const bodyParser = require("body-parser")
 
-app.use(cors({
-    origin: "*"
-}))
+app.use(
+    cors({
+        origin: "*",
+    })
+)
 
-app.use('/forest/assets/avatars', express.static('forest/assets/avatars'))
-app.use('/forest/assets/logos', express.static('forest/assets/logos'))
-app.use('/forest/assets/posts', express.static('forest/assets/posts'))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
-app.use(bodyParser.json({ limit: '50mb' }))
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "forest/")
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        cb(null, Date.now() + ext)
+    },
+})
+const upload = multer({ storage: storage })
+
+app.post("/images", upload.single("image"), (req, res, next) => {
+    if (!req.file) {
+        // Handle case where no file was uploaded
+        return res.status(400).json({ error: "No file uploaded" })
+    }
+    const mimeType = mime.lookup(req.file.originalname)
+
+    if (!mimeType || !mimeType.startsWith("image/")) {
+        // Handle case where uploaded file is not an image
+        return res.status(400).json({ error: "File must be an image" })
+    }
+    const filePath = req.file.path
+
+    res.status(200).json({ success: true })
+})
+
+// Route for retrieving an image
+app.get("/images/:filename", (req, res, next) => {
+    const filePath = path.join(__dirname, "uploads", req.params.filename)
+
+    // Return the image file
+    res.sendFile(filePath)
+})
 
 const jsonErrorHandler = async (err, req, res, next) => {
-    res.status(err.status).send(err);
+    res.status(err.status).send(err)
 }
 app.use(jsonErrorHandler)
 
 app.use(cookieParser())
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     req.user = req.cookies.user
 
     next()
-});
+})
 
-const files = fs.readdirSync('./routes/').filter(file => file.endsWith('.js'))
+const files = fs.readdirSync("./routes/").filter((file) => file.endsWith(".js"))
 
 for (const file of files) {
     const route = require(`./routes/${file}`)
@@ -43,7 +77,3 @@ for (const file of files) {
 }
 
 app.listen(port, () => console.log("Server on: http://localhost:" + port))
-
-function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1800s'})
-}
